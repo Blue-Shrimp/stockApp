da916e17a98de394c4921322755017d1a76e9125
Blue-Shrimp <wkdtmd1202@naver.com>
2022-07-15 17:38:07 +0900

    react-native-config 안드로이드 설정

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import Config from 'react-native-config'
 
 const HomeScreen = () => {
   return (
     <View style={styles.screen}>
       <Text>HomeScreen</Text>
+      <Text>ENV : {Config.ENV}</Text>
+      <Text>APP ID : {Config.APP_ID}</Text>
+      <Text>Build Version : {Config.BUILD_VERSION}</Text>
+      <Text>App Version : {Config.APP_VERSION}</Text>
+      <Text>APP Name: {Config.APP_NAME}</Text>
     </View>
   )
 }
@@ -12,9 +18,9 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: '#ffffff',
     justifyContent: 'center',
     alignContent: 'center',
+    alignItems: 'center',
   },
 })
 
